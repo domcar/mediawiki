6b11c96886c178266f24804f688d092791bc1db5
d-caruso <d-caruso@hotmail.it>
2016-05-12 15:52:45 +0200

    2

--- LocalSettings.php
+++ LocalSettings.php
@@ -9,7 +9,7 @@
 #
 # Further documentation for configuration settings may be found at:
 # https://www.mediawiki.org/wiki/Manual:Configuration_settings
-
+#
 # Protect against web entry
 if ( !defined( 'MEDIAWIKI' ) ) {
 	exit;
